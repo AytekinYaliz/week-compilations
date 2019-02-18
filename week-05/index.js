8b62425bc604a1598b7e64b8ee133dfca234a9a5
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2019-02-18 16:21:46 +0000

    initials

---
+++ week-05/index.js
@@ -0,0 +1,16 @@
+function isPalindrome(str) {
+   // return true/false if the given string parameter is a palindrome one
+}
+
+function getTheSecondLargest(arr) {
+   // return the second largest number in the given array
+}
+
+function isLeapYearWithIfElse(year) {
+   // return true/false if the given year parameter is a leap year
+}
+
+function isLeapYearWithSwitch(year) {
+   // return true/false if the given year parameter is a leap year
+   // Use switch statement
+}
