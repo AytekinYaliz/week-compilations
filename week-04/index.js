32822e3bacc03aef1a4ce799752478a34a235dfc
Aytekin Yaliz <aytekin.yaliz@dentsuaegis.com>
2019-02-18 17:12:02 +0000

    changes

--- week-04/index.js
+++ week-04/index.js
@@ -9,10 +9,29 @@ const milkQuantity = 2;
 
 let totalPriceOfTheBasket = 0;
 
-
 // Your functions will go below this line...
+function calculatePriceWithQuantity(price, quantity) {
+   return price * quantity;
+}
 
+function calculateTotalPriceOfTheBasket(price1, price2, price3) {
+   return price1 + price2 + price3;
+}
 
-console.log( formatThePrice(totalPriceOfTheBasket) );
+function formatThePrice(price) {
+   return "The total price for your basket is: £" + price.toFixed(2);
+}
+
+const priceOfBaget = calculatePriceWithQuantity(bagetPrice, bagetQuantity);
+const priceOfButter = calculatePriceWithQuantity(butterPrice, butterQuantity);
+const priceOfMilk = calculatePriceWithQuantity(milkPrice, milkQuantity);
+
+totalPriceOfTheBasket = calculateTotalPriceOfTheBasket(
+   priceOfBaget,
+   priceOfButter,
+   priceOfMilk
+);
+
+console.log(formatThePrice(totalPriceOfTheBasket));
 
 // The output should be "The total price for your basket is: £5.82"
--- week-05/index.js
+++ week-05/index.js
@@ -1,16 +1,42 @@
 function isPalindrome(str) {
    // return true/false if the given string parameter is a palindrome one
+   let arr = str.split("");
+   arr = arr.reverse();
+   arr = arr.join("");
+
+   return str === arr;
 }
 
 function getTheSecondLargest(arr) {
    // return the second largest number in the given array
+   if (arr.length < 2) {
+      return "Array is too short!";
+   }
+
+   let tempArr = arr.sort();
+
+   return tempArr[tempArr.length - 2];
 }
 
 function isLeapYearWithIfElse(year) {
    // return true/false if the given year parameter is a leap year
+
+   return year % 4 === 0;
 }
 
 function isLeapYearWithSwitch(year) {
    // return true/false if the given year parameter is a leap year
    // Use switch statement
+   let isLeapYear = false;
+
+   switch (year % 4) {
+      case 0:
+         isLeapYear = true;
+         break;
+      default:
+         isLeapYear = false;
+         break;
+   }
+
+   return isLeapYear;
 }
